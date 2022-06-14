d9e7ba30a8e626fcaa29c1058f383e6313270e11
TantalusDenton <thomasdxdenton@hotmail.com>
2022-06-13 23:52:51 -0700

    test methods and detectCapiral use correct

--- src/main/java/CILab.java
+++ src/main/java/CILab.java
@@ -13,7 +13,24 @@ public class CILab implements CILabInterface {
 
     @Override
     public boolean detectCapitalUse() {
-        return false;
+        boolean isCapitalUseRight = false;
+
+        /*for(int i = 0; i < myString.length();i++){
+
+            if(Character.isUpperCase(myString.charAt(1))){
+                if(!Character.isUpperCase(myString.charAt(i)))
+                    isCapitalUseRight = true;
+            }
+
+        }*/
+
+        if(myString.matches("[^a-z]*") ||
+                myString.matches("^[A-Z]+[a-z]*") ||
+                myString.matches("^[a-z]+$") ){
+            isCapitalUseRight = true;
+        }
+
+        return isCapitalUseRight;
     }
 
 }
--- src/test/java/CILabTest.java
+++ src/test/java/CILabTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class CILabTest {
@@ -20,12 +21,20 @@ public class CILabTest {
 
     @Test
     public void detectCapitalUseTest1() {
-       fail("Not yet implemented");
+       myString.setString("FlaG");;
+       assertFalse(myString.detectCapitalUse());
 
     }
     @Test
     public void detectCapitalUseTest2() {
-        fail("Not yet implemented");
+        myString.setString("Usa");;
+        assertTrue(myString.detectCapitalUse());
+    }
+
+    @Test
+    public void detectCapitalUseTest3() {
+        myString.setString("heraldicclouds");;
+        assertTrue(myString.detectCapitalUse());
     }
 
 
